8e2c268a5e4083d8f8f2da575f66c53853d329a0
riadqerimii <123258318+riadqerimii@users.noreply.github.com>
2023-11-27 21:54:23 +0100

    ss111s

--- src/Components/Pages/About/Testimonials.jsx
+++ src/Components/Pages/About/Testimonials.jsx
@@ -2,19 +2,10 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
-import AOS from "aos";
-import "aos/dist/aos.css";
-
 import styles from "./About.module.css"; // Import your styles module
 import { data } from "./Data";
 
 function Testimonials() {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-    });
-  }, []);
-
   const [display, setDisplay] = useState(1);
 
   const [image, setImage] = useState(0);
@@ -46,26 +37,18 @@ function Testimonials() {
           <Col
             md={4}
             sm={12}
-            data-aos="fade-up"
             className={styles.cols}
             onClick={() => showTab(1)}
           >
             Alle Projekte
           </Col>
-          <Col
-            md={4}
-            sm={12}
-            data-aos="fade-up"
-            className={styles.col}
-            onClick={() => showTab(2)}
-          >
+          <Col md={4} sm={12} className={styles.col} onClick={() => showTab(2)}>
             Geneva{" "}
           </Col>
           <Col
             md={4}
             sm={12}
             className={styles.colon}
-            data-aos="fade-up"
             onClick={() => showTab(3)}
           >
             Zurich{" "}
@@ -89,19 +72,17 @@ function Testimonials() {
               </div>
             </Col>
             <Col md={6}>
-              <h1 data-aos="fade-up" className="pt-3">
-                Unsere Projekte
-              </h1>
-              <h3 data-aos="fade-up" className="p-4 ">
+              <h1 className="pt-3">Unsere Projekte</h1>
+              <h3 className="p-4 ">
                 Entdecken Sie die Vielfalt unserer abgeschlossenen Projekte und
                 lassen Sie sich von unserer Expertise inspirieren.
               </h3>
-              <p data-aos="fade-up">
+              <p>
                 Unsere engagierten Teams haben mit Leidenschaft an einer
                 Vielzahl von Projekten gearbeitet, um innovative Lösungen und
                 herausragende Ergebnisse zu erzielen.
               </p>
-              <p data-aos="fade-up">
+              <p>
                 Von Garten- und Landschaftsgestaltung bis hin zu umfangreichen
                 Bauprojekten - unsere Projekte spiegeln unsere Hingabe zu
                 Qualität und Kreativität wider.
@@ -118,26 +99,16 @@ function Testimonials() {
                 alt="someone"
               />
               <div>
-                <button
-                  data-aos="fade-up"
-                  className={` ${styles.next} `}
-                  onClick={prevButton}
-                >
+                <button className={` ${styles.next} `} onClick={prevButton}>
                   ◀
                 </button>
-                <button
-                  data-aos="fade-up"
-                  className={` ${styles.prev} `}
-                  onClick={nextButton}
-                >
+                <button className={` ${styles.prev} `} onClick={nextButton}>
                   ▶
                 </button>
               </div>
             </Col>
             <Col md={6}>
-              <h1 data-aos="fade-up" className="pt-3">
-                Unsere Arbeit in Genf
-              </h1>
+              <h1 className="pt-3">Unsere Arbeit in Genf</h1>
               <h3 className="p-2 ">
                 Entdecken Sie die Vielfalt unserer abgeschlossenen Projekte in
                 Genf und lassen Sie sich von unserer Expertise inspirieren.
@@ -162,13 +133,13 @@ function Testimonials() {
         </div>
         <div className={display === 3 ? styles.show : styles.hide}>
           <Row>
-            <Col md={6} data-aos="fade-up">
+            <Col md={6}>
               <img
                 className={`${styles.imagesAbout}`}
                 src={data[image]}
                 alt="someone"
               />
-              <div data-aos="fade-up">
+              <div>
                 <button className={` ${styles.next} `} onClick={prevButton}>
                   ◀
                 </button>
@@ -178,24 +149,22 @@ function Testimonials() {
               </div>
             </Col>
             <Col md={6}>
-              <h1 data-aos="fade-up" className="pt-3">
-                Unsere Arbeit in Zürich
-              </h1>
-              <h3 data-aos="fade-up" className="p-2 ">
+              <h1 className="pt-3">Unsere Arbeit in Zürich</h1>
+              <h3 className="p-2 ">
                 Entdecken Sie die Vielfalt unserer abgeschlossenen Projekte in
                 Zürich und lassen Sie sich von unserer Expertise inspirieren.
               </h3>
-              <p data-aos="fade-up">
+              <p>
                 Unsere engagierten Teams haben mit Leidenschaft an einer
                 Vielzahl von Projekten in Zürich gearbeitet, um innovative
                 Lösungen und herausragende Ergebnisse zu erzielen.
               </p>
-              <p data-aos="fade-up">
+              <p>
                 Von Garten- und Landschaftsgestaltung bis hin zu umfangreichen
                 Bauprojekten - unsere Projekte in Zürich spiegeln unsere Hingabe
                 zu Qualität und Kreativität wider.
               </p>
-              <p data-aos="fade-up">
+              <p>
                 Wir sind stolz darauf, Teil der Zürcher Gemeinschaft zu sein und
                 durch unsere Arbeit zur Schönheit und Funktionalität der Stadt
                 beizutragen.
