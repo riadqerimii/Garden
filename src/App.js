e9a6a44b6960da3f505851e149aaf3a190933c4e
riadqerimii <123258318+riadqerimii@users.noreply.github.com>
2023-11-27 20:53:45 +0100

    firstt

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import "./App.css";
 import About from "./Components/Pages/About/About.jsx";
 import Contact from "./Components/Pages/Contact/Contact.jsx";
--- src/Components/Pages/About/Data.jsx
+++ src/Components/Pages/About/Data.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const data = [
   "https://images.unsplash.com/uploads/1411901100260f56b39b9/ab70b250?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1516253593875-bd7ba052fbc5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
